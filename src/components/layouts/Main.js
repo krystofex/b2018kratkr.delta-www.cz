e955851897c0b7456f2b08197cf72cfcc4b96470
Kryštof <krystof.kratky2003@gmail.com>
2022-02-01 18:32:20 +0000

    deploy action & preact & html head & favicon

--- src/components/layouts/Main.js
+++ src/components/layouts/Main.js
@@ -1,8 +1,15 @@
 import Navbar from '../modules/Navbar';
+import Head from "next/head"
 
 const MainLayout = ({ children }) => {
   return (
     <div className="flex flex-col items-center justify-between h-screen">
+      <Head>
+        <title>Histories</title>
+        <meta content="Histories landing page." name="description"/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <main className="flex flex-col justify-center items-center w-full h-full">
         {children}
